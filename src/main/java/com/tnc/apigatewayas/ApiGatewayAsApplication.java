64a2eeed7741c02d21a6bb65cfc73fb1a31f13cd
a830083 <bogdan.tancau@atos.net>
2022-06-17 13:25:04 +0300

    add all dependencies and started the application

--- src/main/java/com/tnc/apigatewayas/ApiGatewayAsApplication.java
+++ src/main/java/com/tnc/apigatewayas/ApiGatewayAsApplication.java
@@ -2,12 +2,14 @@ package com.tnc.apigatewayas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayAsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayAsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayAsApplication.class, args);
+    }
 
 }
